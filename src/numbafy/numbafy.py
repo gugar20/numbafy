1887c61db3f27d6755cfe19559dcfb2ec408bbc4
gugar20 <guillaume.gariepy.1@ulaval.ca>
2021-06-20 20:28:15 -0500

    Rearranged for wheel build

--- src/numbafy/numbafy.py
+++ src/numbafy/numbafy.py
@@ -3,32 +3,33 @@ import sympy as sym
 from numba import jit
 import numpy as np
 
+
 def numbafy(expression, parameters=None, constants=None, use_cse=False, new_function_name='numbafy_func'):
     cse = sym.cse(expression)
-    
+
     code_parameters = ''
     code_constants = ''
     code_cse = ''
 
     if parameters:
         code_parameters = ', '.join(f'{p}' for p in parameters)
-    
+
     if constants:
         code_constants = []
         for k, v in constants.items():
             code_constants.append(f'{k} = {v}')
         code_constants = '\n    '.join(code_constants)
-        
+
     if use_cse:
         shape = np.shape(expression)
-        if len(shape)==1:
+        if len(shape) == 1:
             expressions = sym.cse(expression)
             expressions_shaped = expressions[1]
             code_expression = f"""{expressions_shaped}"""
-        elif len(shape)==2:
+        elif len(shape) == 2:
             expressions = sym.cse(np.ravel(expression))
             expressions_shaped = sym.Matrix(expressions[1]).reshape(*shape)
-            code_expression = f"""{expressions_shaped}"""[7:][:-1] 
+            code_expression = f"""{expressions_shaped}"""[7:][:-1]
         else:
             raise TypeError
         code_cse = []
@@ -36,16 +37,16 @@ def numbafy(expression, parameters=None, constants=None, use_cse=False, new_func
             k, v = e
             code_cse.append(f'{k} = {v}')
         code_cse = '\n    '.join(code_cse)
-        #f'{expressions[1][0]}'
+        # f'{expressions[1][0]}'
     else:
         code_expression = f'return {expression}'
-        
+
     template = f"""@jit(nopython=True)
 def {new_function_name}({code_parameters}):
     {code_constants}
     {code_cse}
     return {code_expression}"""
-       
+
     return template
 
 
